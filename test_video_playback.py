635799e45a79610785532e49bd6a17d0999bede9
Aravind Battaje <aravind@oxidification.com>
2017-10-16 11:06:22 -0400

    Add commandline args to test_video_playback
    Just to make it a little friendly and easy to use.

--- test_video_playback.py
+++ test_video_playback.py
@@ -1,49 +1,90 @@
 import cv2
 from utils.input import Video
 from utils.display import Display
+import argparse
+import os
 
-video = Video('captures/GOPR8960.MP4')
-display = Display()
+# Scheme - 1
+#     Vanilla playback
 
-# # Scheme - 1
-# #     Vanilla playback
 
-# # Emulate a do-while loop
-# # with "while True" and breaking
-# # if condition fails after executing
-# while True:
-#     frame = video.next_frame()
-#     if video.end_reached():
-#         break
+def scheme_1(file_name):
+    video = Video(file_name)
+    display = Display()
 
-#     # Do some operation
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Emulate a do-while loop
+    # with "while True" and breaking
+    # if condition fails after executing
+    while True:
+        frame = video.next_frame()
+        if video.end_reached():
+            break
 
-#     # Refresh display with new image
-#     display.refresh(gray)
-#     if display.can_quit():
-#         break
+        # Do some operation
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Refresh display with new image
+        display.refresh(gray)
+        if display.can_quit():
+            break
+
 
 # Scheme - 2
 #   Optimized for sequential playback
 #   But with random seeks, playback will be slow
+def scheme_2(file_name):
+    video = Video(file_name)
+    display = Display()
 
-# First get the number of frames
-num_frames = video.get_num_frames()
+    # First get the number of frames
+    num_frames = video.get_num_frames()
 
-# Get each frame from video and display
-# If step is greater than one (simulating random
-# seeks,) the playback will be slow
-for i in range(num_frames):
-    # Get the frame desired
-    frame = video.get_frame(i)
+    # Get each frame from video and display
+    # If step is greater than one (simulating random
+    # seeks,) the playback will be slow
+    for i in range(num_frames):
+        # Get the frame desired
+        frame = video.get_frame(i)
 
-    # Do some operation
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Do some operation
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Refresh display with new image
-    display.refresh(gray)
+        # Refresh display with new image
+        display.refresh(gray)
 
-    # Quit midway if required
-    if display.can_quit():
-        break
+        # Quit midway if required
+        if display.can_quit():
+            break
+
+
+def get_args():
+    # Setup argument parser
+    # and parse the inputs
+    arg_parser = argparse.ArgumentParser()
+
+    arg_parser.add_argument(
+        '-v', '--video', required=True, help='Input video file')
+    arg_parser.add_argument('-s', '--scheme', required=True, type=int,
+                            help='Scheme to run (1 or 2)')
+    args = vars(arg_parser.parse_args())
+
+    if not os.path.exists(args['video']):
+        arg_parser.error('{} file not found'.format(args['video']))
+
+    if args['scheme'] != 1 and args['scheme'] != 2:
+        arg_parser.error('{} not valid scheme'.format(args['scheme']))
+
+    return args
+
+
+def main():
+    args = get_args()
+
+    if args['scheme'] == 1:
+        scheme_1(args['video'])
+    else:
+        scheme_2(args['video'])
+
+
+if __name__ == '__main__':
+    main()
